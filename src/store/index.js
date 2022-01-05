f17c9394f86bf599fb6fcde4d4c83ff8f440ea03
Sean Wong <seanwonghh93@gmail.com>
2022-01-05 11:48:52 +0800

    lesson 5
    - logout function

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ import { createStore } from 'vuex'
 import { auth } from '../firebase/config'
 import {
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut
 } from 'firebase/auth'
 
 const store = createStore({
@@ -43,6 +44,13 @@ const store = createStore({
             } else {
                 throw new Error('could not complete login')
             }
+        },
+
+        async logout(context) {
+            console.log('logout action')
+
+            await signOut(auth)
+            context.commit('setUser', null)
         }
     }
 })
